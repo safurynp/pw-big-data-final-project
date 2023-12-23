6fc273b1921c1798af299bbe4b83c8da21b3b68c
safurynp <safurynp@gmail.com>
2023-12-23 16:10:42 +0000

    Notes in report, bibligraphy setup and tennis data setup as Git submodules

---
+++ src/aws/upload_to_s3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Directory containing the CSV files
+dir="/path/to/your/csv/files"
+
+# Name of your S3 bucket
+bucket="bucket-name"
+
+# Loop over all CSV files in the directory
+for file in $dir/*.csv
+do
+  # Upload the file to the S3 bucket
+  aws s3 cp $file s3://$bucket/
+done
